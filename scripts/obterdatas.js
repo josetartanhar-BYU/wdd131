12118f16a562ef8e882da4613a6278d23178e457
josetartanhar-byu <jcostadasilva@byupathway.edu>
2026-09-03 14:48:03 -0300

    atualização 2

---
+++ scripts/obterdatas.js
@@ -0,0 +1,5 @@
+const hoje = new Date();
+document.getElementById("anoatual").textContent = hoje.getFullYear();
+document.getElementById("ultimaModificacao").textContent = `Última Modificação: ${document.lastModified}`;
+
+
