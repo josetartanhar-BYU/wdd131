8de6bf4b943df718f7860b2d2dd52802546f12db
josetartanhar-byu <jcostadasilva@byupathway.edu>
2026-09-06 15:55:41 -0300

    semana02- parte2

---
+++ semana02/scripts/bom.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('#favchap');
+
+const botao = document.querySelector('button');
+
+const lista = document.querySelector('#list');
+
+botao.addEventListener('click', function(){
+
+    if(input.value.trim() !== ''){
+
+        const li = document.createElement('li');
+
+        const botaoExcluir = document.createElement('button');
+
+        li.textContent = input.value;
+
+        input.value = '';
+
+        
+        botaoExcluir.textContent = '❌';
+
+            li.append(botaoExcluir);
+
+            lista.append(li);
+
+        botaoExcluir.addEventListener('click', function () {
+
+            lista.removeChild(li);
+
+            input.focus();
+
+        });
+
+    }
+    
+    input.focus();
+
+
+})
+
+
+
