d992eb1fb2405a60e47f6d99db36c743b0214ca4
josetartanhar-byu <jcostadasilva@byupathway.edu>
2026-09-07 17:43:32 -0300

    projeto semana 3 - parte2

---
+++ scripts/local.js
@@ -0,0 +1,20 @@
+const temperatura = 8;
+const velocidadeVento = 10;
+
+function calcularSensacaoTermica(temperatura, velocidadevento) {
+    return 13.12 + 0.6215 * temperatura - 11.37 * Math.pow(velocidadeVento, 0.16) + 0.3965 * temperatura * Math.pow(velocidadeVento, 0.16)
+}
+
+if (temperatura <= 10 && velocidadeVento > 4.8) {
+    const resultado = calcularSensacaoTermica(temperatura, velocidadeVento);
+    document.querySelector("#sensacao").textContent = `${resultado.toFixed(1)} °C`;
+
+} else {
+    document.querySelector("#sensacao").textContent = "N/A";
+}
+
+document.querySelector("#anoatual").textContent = new Date().getFullYear();
+
+document.querySelector("#ultimaModificacao").textContent = `Última Modificação: ${document.lastModified}`;
+
+
