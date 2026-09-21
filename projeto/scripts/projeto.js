3a9d1e910eddeb7cb5d8ccd665150eb63bca73c2
josetartanhar-byu <jcostadasilva@byupathway.edu>
2026-09-21 15:03:20 -0300

    projeto final parte 3

--- projeto/scripts/projeto.js
+++ projeto/scripts/projeto.js
@@ -5,7 +5,7 @@ const produtos = [
         imagem: "imagens/salgados.webp"
     },
     {
-        nome: "Doces e Salgados",
+        nome: "Doces e Bolos",
         descricao: "Doces e bolos preparados para deixar cada comemoração ainda mais especial. ",
         imagem: "imagens/doces.webp"
     },
@@ -32,4 +32,18 @@ produtos.forEach(function(produto) {
         `;
 
         container.append(card);
+});
+
+const anoAtual = document.querySelector("#anoatual");
+anoAtual.textContent = new Date().getFullYear();
+
+const ultimaModificacao = document.querySelector("#ultimaModificacao");
+ultimaModificacao.textContent = `Última modificação: ${document.lastModified}`;
+
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", function(){
+    nav.classList.toggle("open");
+    menu.classList.toggle("open");
 });
